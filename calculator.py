9f9c51f456e50a3b4eb4c5cc2087a3210127d2de
Uyen Tran <tranphuonguyen1011@gmail.com>
2026-06-21 21:21:27 +0100

    This plan creates a new calculator.py file in the root directory and imp

---
+++ calculator.py
@@ -0,0 +1,12 @@
+def subtract(a, b):
+    """
+    Subtract b from a and return the result.
+    
+    Args:
+        a: The minuend (number to subtract from)
+        b: The subtrahend (number to subtract)
+    
+    Returns:
+        The difference a - b
+    """
+    return a - b
